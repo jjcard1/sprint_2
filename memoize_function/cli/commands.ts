1e362c937e06296a62167e975182288f952e0f3e
Juanjo <juanjo.card.m@gmail.com>
2023-07-27 10:09:46 +0200

    cli memoize

--- memoize_function/cli/commands.ts
+++ memoize_function/cli/commands.ts
@@ -3,15 +3,6 @@ import { program } from "commander"
 const controllers = require('./controllers/memoize.controllers')
 
 const executeFunction = () => {
-
-    const enterNumbers = [
-        {
-            type: 'number',
-            name: 'num',
-            message: 'Enter a number'
-        }
-    ]
-
     program
         .version('1.0.0')
         .description('This CLI calculate the square root of the given number. If the given number is repeated, the answer is going to take the data from cache')
